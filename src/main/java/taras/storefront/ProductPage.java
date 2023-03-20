cc597e10dad309fa8122d3ad55118a5e401c2c89
TarasYanUA <wildmonach@gmail.com>
2023-03-20 13:02:20 +0200

    Product page Var1 is ready!

--- src/main/java/taras/storefront/ProductPage.java
+++ src/main/java/taras/storefront/ProductPage.java
@@ -1,5 +1,9 @@
 package taras.storefront;
 
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.WheelInput;
+import taras.constants.DriverProvider;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import taras.constants.AbstractPage;
@@ -10,12 +14,33 @@ public class ProductPage extends AbstractPage {
     private WebElement gearwheel_Language;
     @FindBy(css = "a[data-ca-name=\"ru\"]")
     private WebElement language_RU;
+    @FindBy(css = "a[data-ca-name=\"ar\"]")
+    private WebElement language_RTL;
     @FindBy(css = "#features a")
     public WebElement tab_Features;
+    @FindBy(css = "a[id*='opener_ut2_features_dialog_']")
+    public WebElement featureDescription;
 
 
     public void shiftLanguage_RU(){
+        ((JavascriptExecutor) DriverProvider.getDriver()).executeScript("scroll(0,0);");
         gearwheel_Language.click();
         language_RU.click();
+    }
+    public void shiftLanguage_RTL(){
+        ((JavascriptExecutor) DriverProvider.getDriver()).executeScript("scroll(0,0);");
+        gearwheel_Language.click();
+        language_RTL.click();
+    }
+
+    public WebElement hoverTab_Features(){
+        return tab_Features;
+    }
+    public void scrollToAndClickTab_Features(){
+        WebElement element = hoverTab_Features();
+        Actions scroll = new Actions(DriverProvider.getDriver());
+        scroll.scrollFromOrigin(WheelInput.ScrollOrigin.fromElement(element), 0, 750);
+        scroll.perform();
+        tab_Features.click();
     }
 }

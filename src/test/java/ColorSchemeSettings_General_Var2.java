1d19de7985f13edab2f5b671b6f258962cbd0bbf
TarasYanUA <wildmonach@gmail.com>
2023-04-18 17:44:56 +0300

    The last test-case for ColorScheme settings is ready!

--- src/test/java/ColorSchemeSettings_General_Var2.java
+++ src/test/java/ColorSchemeSettings_General_Var2.java
@@ -7,28 +7,28 @@ import taras.storefront.StHomePage;
 
 /*
 1) CS-Cart настройки -- Внешний вид:
-Показывать мини-иконки в виде галереи --  нет
+Показывать мини-иконки в виде галереи --  да
 Включить быстрый просмотр -- да
 
 2) UniTheme2 -- Настройки цветосхемы -- вкладка "Общее":
-Скруглить углы для элементов интерфейса --  Полностью скруглить
-Скруглить углы блоков, окон, баннеров --    да
-Отображать заголовки заглавными буквами --  да
-Стиль --    Использовать фоновую заливку
-Отображать текст заглавными буквами --  да
-Отображать тень --  нет
-Добавить объем --   нет
-Иконка Корзины -- Вариант 1
+Скруглить углы для элементов интерфейса --  Не использовать
+Скруглить углы блоков, окон, баннеров --    нет
+Отображать заголовки заглавными буквами --  нет
+Стиль --    Использовать только контур
+Отображать текст заглавными буквами --  нет
+Отображать тень --  да
+Добавить объем --   да
+Иконка Корзины -- Вариант 7
 */
 
-public class ColorSchemeSettings_General_Var1 extends TestRunner{
+public class ColorSchemeSettings_General_Var2 extends TestRunner{
     @Test(priority = 1)
-    public void setConfigurationsFor_ColorSchemeSettings_General_Var1() {
+    public void setConfigurationsFor_ColorSchemeSettings_General_Var2() {
         //Настраиваем CS-Cart настройки
         CsCartSettings csCartSettings = new CsCartSettings();
         csCartSettings.navigateToAppearanceSettingsOfCsCart();
         WebElement checkboxThumbnailsGallery = csCartSettings.setting_ThumbnailsGallery;
-        if (checkboxThumbnailsGallery.isSelected()) {
+        if (!checkboxThumbnailsGallery.isSelected()) {
             checkboxThumbnailsGallery.click();
         }
         WebElement checkboxSettingQuickView = csCartSettings.setting_QuickView;
@@ -42,30 +42,30 @@ public class ColorSchemeSettings_General_Var1 extends TestRunner{
         colorSchemeSettings.fieldOfActiveColorScheme.click();
         colorSchemeSettings.activeColorScheme.click();
         makePause();
-        colorSchemeSettings.selectSetting_General_RoundCornersForElements("full");
-        if(!colorSchemeSettings.setting_General_RoundCornersOfBlocks.isSelected()){
+        colorSchemeSettings.selectSetting_General_RoundCornersForElements("do_not_use");
+        if(colorSchemeSettings.setting_General_RoundCornersOfBlocks.isSelected()){
             colorSchemeSettings.setting_General_RoundCornersOfBlocks.click();
         }
-        if(!colorSchemeSettings.setting_General_DisplayHeadersInCapitalLetters.isSelected()){
+        if(colorSchemeSettings.setting_General_DisplayHeadersInCapitalLetters.isSelected()){
             colorSchemeSettings.setting_General_DisplayHeadersInCapitalLetters.click();
         }
-        colorSchemeSettings.selectSetting_General_ButtonsStyle("use_background");
-        if(!colorSchemeSettings.setting_General_DisplayTextInCapitalLetters.isSelected()){
+        colorSchemeSettings.selectSetting_General_ButtonsStyle("use_border");
+        if(colorSchemeSettings.setting_General_DisplayTextInCapitalLetters.isSelected()){
             colorSchemeSettings.setting_General_DisplayTextInCapitalLetters.click();
         }
-        if(colorSchemeSettings.setting_General_AddShadow.isSelected()){
+        if(!colorSchemeSettings.setting_General_AddShadow.isSelected()){
             colorSchemeSettings.setting_General_AddShadow.click();
         }
-        if(colorSchemeSettings.setting_General_AddBulk.isSelected()){
+        if(!colorSchemeSettings.setting_General_AddBulk.isSelected()){
             colorSchemeSettings.setting_General_AddBulk.click();
         }
-        colorSchemeSettings.selectSetting_General_CartIcon("type1");
+        colorSchemeSettings.selectSetting_General_CartIcon("type7");
         csCartSettings.clickSaveButtonOfSettings();
     }
 
-    @Test(priority = 2, dependsOnMethods = "setConfigurationsFor_ColorSchemeSettings_General_Var1",
+    @Test(priority = 2, dependsOnMethods = "setConfigurationsFor_ColorSchemeSettings_General_Var2",
     description = "Здесь проверок нет, так как настройки цветосхемы отсутствуют в коде")
-    public void checkColorSchemeSettings_General_Var1(){
+    public void checkColorSchemeSettings_General_Var2(){
         CsCartSettings csCartSettings = new CsCartSettings();
         StHomePage stHomePage = csCartSettings.navigateToStorefrontMainPage();
         focusBrowserTab(1);
@@ -73,30 +73,30 @@ public class ColorSchemeSettings_General_Var1 extends TestRunner{
         //Блок товаров на главной странице
         stHomePage.scrollToBlockWithProducts();
         stHomePage.hoverToProductInProductBlock();
-        takeScreenShot_withoutScroll("2100 ColorScheme, General, Var1 - Block with products");
+        takeScreenShot_withoutScroll("2150 ColorScheme, General, Var2 - Block with products");
         stHomePage.selectLanguage_RTL();
         stHomePage.scrollToBlockWithProducts();
         stHomePage.hoverToProductInProductBlock();
-        takeScreenShot_withoutScroll("2105 ColorScheme, General, Var1 - Block with products (RTL)");
+        takeScreenShot_withoutScroll("2155 ColorScheme, General, Var2 - Block with products (RTL)");
         stHomePage.selectLanguage_RU();
 
         //Категория "Телефоны"
         stHomePage.navigateToMenuPhones();
         StCategoryPage stCategoryPage = new StCategoryPage();
         stCategoryPage.hoverToPhoneProduct();
-        takeScreenShot("2110 ColorScheme, General, Var1 - Category page");
+        takeScreenShot("2160 ColorScheme, General, Var2 - Category page");
         stCategoryPage.clickQuickViewOfPhoneProduct();
-        takeScreenShot("2115 ColorScheme, General, Var1 - QuickView");
+        takeScreenShot("2165 ColorScheme, General, Var2 - QuickView");
         stCategoryPage.clickButton_WriteReview();
-        takeScreenShot("2120 ColorScheme, General, Var1 - Write review");
+        takeScreenShot("2170 ColorScheme, General, Var2 - Write review");
         stCategoryPage.closeWriteReview.click();
         stCategoryPage.clickCloseQuickView();
         stHomePage.selectLanguage_RTL();
         stCategoryPage.hoverToPhoneProduct();
-        takeScreenShot("2125 ColorScheme, General, Var1 - Category page (RTL)");
+        takeScreenShot("2175 ColorScheme, General, Var2 - Category page (RTL)");
         stCategoryPage.clickQuickViewOfPhoneProduct();
-        takeScreenShot("2130 ColorScheme, General, Var1 - QuickView (RTL)");
+        takeScreenShot("2180 ColorScheme, General, Var2 - QuickView (RTL)");
         stCategoryPage.clickButton_WriteReview();
-        takeScreenShot("2135 ColorScheme, General, Var1 - Write review (RTL)");
+        takeScreenShot("2185 ColorScheme, General, Var2 - Write review (RTL)");
     }
 }

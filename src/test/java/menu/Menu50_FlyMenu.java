921aa4d62709319726b8fd39a283c7016be05da7
Taras-Mila <w@gmail.com>
2023-08-25 13:28:33 +0300

    Added asserts to the first test-case of Fly menu

--- src/test/java/menu/Menu50_FlyMenu.java
+++ src/test/java/menu/Menu50_FlyMenu.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import taras.adminPanel.CsCartSettings;
 import taras.adminPanel.MenuSettings;
 import taras.constants.DriverProvider;
@@ -51,6 +52,16 @@ public class Menu50_FlyMenu extends TestRunner{
         stHomePage.button_FlyMenu.click();
         (new WebDriverWait((DriverProvider.getDriver()), Duration.ofSeconds(4)))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ut2-lfl.ty-menu-item__products p")));
+        SoftAssert softAssert = new SoftAssert();
+        //Проверяем, что Кол-во отображаемых элементов во 2-м уровне меню -- 5
+        softAssert.assertTrue(DriverProvider.getDriver().findElements(By.cssSelector(".ty-menu-item__electronics div[class='ut2-lsl with-pic']")).size() ==5,
+                "'Number of visible elements in the 2-level menu' is not 5!");
+        //Проверяем, что присутствует кнопка "Больше [категория]"
+        softAssert.assertTrue(DriverProvider.getDriver().findElements(By.cssSelector(".ut2-lsl__show_more")).size() >=1,
+                "There is no any button 'More [category]' in the second level menu!");
+        //Проверяем, что Кол-во отображаемых элементов в 3-м уровне меню -- не меньше 7
+        softAssert.assertTrue(DriverProvider.getDriver().findElements(By.cssSelector(".ty-menu-item__electronics .ut2-tlw a[href*='kompyutery']")).size() >=7,
+                "'Number of visible elements in the 3-level menu' is less than 7!");
         stHomePage. navigateToFlyMenu_AllProducts();
         takeScreenShot("Menu50.00 Menu50_FlyMenu - Menu AllProducts");
         stHomePage.navigateToFlyMenu_Electronics();

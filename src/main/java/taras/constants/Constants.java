2426cedafc7e3fb023aa85c12dc0b5806858e49b
TarasYanUA <wildmonach@gmail.com>
2023-10-09 15:33:29 +0300

    Fixed css selectors for three menu blocks in the layout pages.

--- src/main/java/taras/constants/Constants.java
+++ src/main/java/taras/constants/Constants.java
@@ -13,5 +13,5 @@ public class Constants {
     public static final String OPERA_DRIVER_PROPERTY = "webdriver.edge.driver";
     public static final String OPERA_DRIVER_PATH = "src/test/resources/operadriver.exe";
 
-    public static final String BASIC_URL = "https://trs.test.abt.team/4171ultru/admin.php?dispatch=addons.manage";
+    public static final String BASIC_URL = "https://trs.test.abt.team/4171ultru_seo_for_blog/admin.php?dispatch=addons.manage";
 }

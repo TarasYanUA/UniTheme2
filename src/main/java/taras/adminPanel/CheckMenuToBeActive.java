3e3f05f49f6a8017814e96d03992052979d8776f
Taras-Mila <w@gmail.com>
2024-05-03 17:17:11 +0300

    Adaptation to test platform with the AB: Add-ons.

--- src/main/java/taras/adminPanel/CheckMenuToBeActive.java
+++ src/main/java/taras/adminPanel/CheckMenuToBeActive.java
@@ -7,8 +7,17 @@ import taras.constants.DriverProvider;
 public interface CheckMenuToBeActive {
 
     default void checkMenuToBeActive(WebElement menu) {
+        if(DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../..//a[contains(@class, ' collapsed')]")).isEmpty()) {
+            DriverProvider.getDriver().findElement(By.xpath("//span[text()='_ab__addons']")).click();
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
         if(DriverProvider.getDriver().findElements(By.xpath(String.valueOf(menu))
-                .xpath("/../..//a[contains(@class, 'main-menu-1__toggle--active')]")).isEmpty()) {
+                .xpath("/../..//a[contains(@class, ' collapsed')]")).isEmpty()) {
             menu.click();
             try {
                 Thread.sleep(1500);

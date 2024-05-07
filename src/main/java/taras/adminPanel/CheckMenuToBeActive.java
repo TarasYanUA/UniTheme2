d6ef53a00acd4955bbda7c9997698efc83a22c35
TarasYanUA <wildmonach@gmail.com>
2024-05-07 16:32:42 +0300

    Some changes for product page!

--- src/main/java/taras/adminPanel/CheckMenuToBeActive.java
+++ src/main/java/taras/adminPanel/CheckMenuToBeActive.java
@@ -6,34 +6,6 @@ import taras.constants.DriverProvider;
 import java.util.List;
 
 public interface CheckMenuToBeActive {
-    static void checkMenu_Addons_ToBeActive() {
-
-        // Проверяем наличие хотя бы одного из элементов
-        List<WebElement> firstElement = DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../../..//div[contains(@class, 'in collapse')]"));
-        List<WebElement> secondElement = DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../..//a[contains(@class, 'main-menu-1__toggle--active')]"));
-
-        if (!firstElement.isEmpty() || !secondElement.isEmpty()) {
-            // Если хотя бы один элемент присутствует, выполняем действие
-            DriverProvider.getDriver().findElement(By.xpath("//span[text()='_ab__addons']")).click();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-        /*if(!DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../../..//div[contains(@class, 'in collapse')]")).isEmpty() ||
-                !DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../..//a[contains(@class, 'main-menu-1__toggle--active')]")).isEmpty()) {
-            DriverProvider.getDriver().findElement(By.xpath("//span[text()='_ab__addons']")).click();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }*/
-
-
     default void checkMenuToBeActive(WebElement menu) {
         checkMenu_Addons_ToBeActive();
         if (DriverProvider.getDriver().findElements(By.xpath(String.valueOf(menu))
@@ -52,5 +24,22 @@ public interface CheckMenuToBeActive {
         checkMenu_Addons_ToBeActive();
         if (DriverProvider.getDriver().findElements(By.cssSelector("a[href$='products.manage'].main-menu-1__link ~ a[class*='main-menu-1__toggle--active']")).isEmpty())
             DriverProvider.getDriver().findElement(By.cssSelector("a[href$='products.manage'].main-menu-1__link")).click();
+    }
+
+
+    static void checkMenu_Addons_ToBeActive() {
+        // Проверяем наличие хотя бы одного из элементов
+        List<WebElement> firstElement = DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../../..//div[contains(@class, 'in collapse')]"));
+        List<WebElement> secondElement = DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../..//a[contains(@class, 'main-menu-1__toggle--active')]"));
+
+        if (!firstElement.isEmpty() || !secondElement.isEmpty()) {
+            // Если хотя бы один элемент присутствует, выполняем действие
+            DriverProvider.getDriver().findElement(By.xpath("//span[text()='_ab__addons']")).click();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }

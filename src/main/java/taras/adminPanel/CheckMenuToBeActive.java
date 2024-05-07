73a270adcc6f2e3abc233ee709956069881ad2ac
TarasYanUA <wildmonach@gmail.com>
2024-05-07 12:15:38 +0300

    I wanted to increase speed for Condition of the menu element ab_addons but it is not possible because the system needs time to write a proper code. But I improved the visibility of this method.

--- src/main/java/taras/adminPanel/CheckMenuToBeActive.java
+++ src/main/java/taras/adminPanel/CheckMenuToBeActive.java
@@ -3,11 +3,17 @@ package taras.adminPanel;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import taras.constants.DriverProvider;
+import java.util.List;
 
 public interface CheckMenuToBeActive {
-    static void checkMenu_Addons_ToBeActive(){
-        if(!DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../../..//div[contains(@class, 'in collapse')]")).isEmpty() ||
-                !DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../..//a[contains(@class, 'main-menu-1__toggle--active')]")).isEmpty()) {
+    static void checkMenu_Addons_ToBeActive() {
+
+        // Проверяем наличие хотя бы одного из элементов
+        List<WebElement> firstElement = DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../../..//div[contains(@class, 'in collapse')]"));
+        List<WebElement> secondElement = DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../..//a[contains(@class, 'main-menu-1__toggle--active')]"));
+
+        if (!firstElement.isEmpty() || !secondElement.isEmpty()) {
+            // Если хотя бы один элемент присутствует, выполняем действие
             DriverProvider.getDriver().findElement(By.xpath("//span[text()='_ab__addons']")).click();
             try {
                 Thread.sleep(1000);
@@ -17,23 +23,34 @@ public interface CheckMenuToBeActive {
         }
     }
 
+        /*if(!DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../../..//div[contains(@class, 'in collapse')]")).isEmpty() ||
+                !DriverProvider.getDriver().findElements(By.xpath("//span[text()='_ab__addons']/../..//a[contains(@class, 'main-menu-1__toggle--active')]")).isEmpty()) {
+            DriverProvider.getDriver().findElement(By.xpath("//span[text()='_ab__addons']")).click();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }*/
+
+
     default void checkMenuToBeActive(WebElement menu) {
         checkMenu_Addons_ToBeActive();
-        if(DriverProvider.getDriver().findElements(By.xpath(String.valueOf(menu))
+        if (DriverProvider.getDriver().findElements(By.xpath(String.valueOf(menu))
                 .xpath("/../..//a[contains(@class, 'active')]")).isEmpty()) {
-                menu.click();
-                try {
-                    Thread.sleep(1500);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            menu.click();
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 
         }
     }
 
     default void checkMenu_Products_ToBeActive() {
         checkMenu_Addons_ToBeActive();
-        if(DriverProvider.getDriver().findElements(By.cssSelector("a[href$='products.manage'].main-menu-1__link ~ a[class*='main-menu-1__toggle--active']")).isEmpty())
+        if (DriverProvider.getDriver().findElements(By.cssSelector("a[href$='products.manage'].main-menu-1__link ~ a[class*='main-menu-1__toggle--active']")).isEmpty())
             DriverProvider.getDriver().findElement(By.cssSelector("a[href$='products.manage'].main-menu-1__link")).click();
     }
 }

08c6329878f809f2cd50eb8dd66ee1ad1c91d115
Taras-Mila <w@gmail.com>
2024-06-14 18:56:06 +0300

    Добавил настройку "Настройки темы -- Вкладка "Показать ещё" -- Разрешить для товарных списков"

--- src/test/java/GeneralSettings_ProductLists_GridListView_Var1.java
+++ src/test/java/GeneralSettings_ProductLists_GridListView_Var1.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.asserts.SoftAssert;
 import org.testng.annotations.Test;
 import taras.adminPanel.ThemeSettings_ProductLists;
+import taras.adminPanel.ThemeSettings_ShowMore;
 import taras.constants.DriverProvider;
 import taras.adminPanel.CsCartSettings;
 import taras.storefront.StCategoryPage;
@@ -29,6 +30,8 @@ import java.time.Duration;
 Отображать "Вы экономите" -- да
 Показывать галерею мини-иконок товара в товарном списке --	Не отображать
 Переключать изображение товара при движении мышки -- с полосками
+
+Вкладка "Показать ещё" -- Разрешить для товарных списков -- нет
 */
 
 public class GeneralSettings_ProductLists_GridListView_Var1 extends TestRunner {
@@ -90,6 +93,10 @@ public class GeneralSettings_ProductLists_GridListView_Var1 extends TestRunner {
         }
         themeSettingsProductLists.selectSetting_ShowGalleryOfMiniIcons("N");
         themeSettingsProductLists.selectSetting_SwitchProductImageWhenHovering("lines");
+        ThemeSettings_ShowMore themeSettings_showMore = new ThemeSettings_ShowMore();
+        themeSettings_showMore.navigateTo_ThemeSettings_tabShowMore();
+        if(themeSettings_showMore.setting_AllowForProductLists.isSelected())
+            themeSettings_showMore.setting_AllowForProductLists.click();
         csCartSettings.clickSaveButtonOfSettings();
     }
 
